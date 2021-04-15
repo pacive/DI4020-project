e8062e262844bb2836ff46bbe770f6cef714cf23
Linnea Wall <llinneawall@gmail.com>
2021-04-15 22:49:08 +0200

    beginning of template.php

---
+++ template.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>SmartHome.net</title>
+    <link rel="stylesheet" href="css/page.css">
+    <script defer src="media/js/app.js"></script>
+  </head>
+  <body>
+      <div class="content">
+        <h1 id="header">Smarthome/or name of the house?</h1>
+          <div id="sideBar" class="sideBar">
+              <p id="close">&times;</p>
+              <div class="menu"> 
+                  <!-- <div>s and <p>s for the rooms will be added here -->
+              </div>
+              <div class="admin">
+                   <!-- options for admin will be added here-->
+              </div>
+              <p id="logout">Logout</p>
+          </div>
+          <p id="open">&#9776;</p>
+
+          <div id="image" class ="image">
+            <img src="https://media.discordapp.net/attachments/801081934410678302/826815048143208468/notstolen.png?width=604&height=415" alt="Layout sketch" usemap="#blueprint"/>
+            <map id="blueprint" name="blueprint">
+            </map>
+          </div>
+        </div>
+  </body>
+</html>
