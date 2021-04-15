c29ac90252b83d994c7f35e6ca93d5744c25e79b
Anders Alfredsson <30875102+pacive@users.noreply.github.com>
2021-04-15 15:26:28 +0200

    Add code comments to app.js
    Signed-off-by: Anders Alfredsson <andersb86@gmail.com>

--- media/js/app.js
+++ media/js/app.js
@@ -1,6 +1,86 @@
 const PROJECT_ROOT = "/~andalf20/project/";
 const API_BASE = PROJECT_ROOT + "api/";
 
+
+/*
+ * Call init() function after page has finished loading
+ */
+window.addEventListener('load', () => {
+  init();
+});
+
+/*
+ * Initialize the page by downloading data, adding event listeners,
+ * and creating elements
+ */
+function init() {
+  // Get all rooms from the api
+  getAll("rooms", "?includeDevices=true", (status, data) => {
+    if (status == 200) {
+      // Save the rooms in the browser
+      sessionStorage.setItem('rooms', data);
+      let rooms = JSON.parse(data);
+      // Create an area for each room in the map
+      rooms.forEach(room => {
+        createArea(room);
+      });
+      // Start listening for status updates
+      startSse();
+    }
+  });
+
+  // Resize the areas so they always match the image size if it's changed, e.g if changing to portrait view on a phone
+  window.addEventListener('resize', (e) => {
+    let mapElem = document.querySelector('.image map');
+    while (first = mapElem.firstChild) {
+      mapElem.removeChild(first);
+    }
+    let rooms = JSON.parse(sessionStorage.getItem('rooms'));
+    rooms.forEach(room => {
+      createArea(room);
+    });
+  });
+
+  // Add event listener to popup close button
+  document.getElementById('closepopup')?.addEventListener('click', () => {
+    document.getElementById('roompopup').style.visibility = 'hidden';
+  });  
+
+  // Add event listeners to login form if at the login page
+  let loginForm = document.getElementById("login");
+  if (loginForm !== null) {
+    loginForm.querySelector('#submit').addEventListener('click', login);
+    document.addEventListener('keypress', function(ev) { ev.key == 'Enter' ? login() : null; });
+  }
+
+  // Add event listeners to menu open and close buttons
+  document.getElementById('open')?.addEventListener('click', openBar);
+  document.getElementById('close')?.addEventListener('click', closeBar);
+}
+
+/*
+ * Start subscribing to events from server and updates element
+ */
+function startSse() {
+  var events = new EventSource(API_BASE + "events.php");
+  events.onmessage = (event) => {
+    console.log(event.data);
+    let data = JSON.parse(event.data);
+    sessionStorage.setItem('device-' + data.id, data.status);
+    let p = document.getElementById("device-" + data.id);
+    if (p !== null) {
+      p.querySelector('input').checked = data.status == 'ON';
+    }
+  }
+}
+
+  /*
+   * DOM manipulation
+   */
+
+/*
+ * Open the menu
+ */
 function openBar() {
   if (window.matchMedia('(orientation:portrait)').matches) {
     document.getElementById("sideBar").style.width = "40%";
@@ -9,10 +89,17 @@ function openBar() {
   }
 }
 
+/*
+ * close the menu
+ */
 function closeBar() {
   document.getElementById("sideBar").style.width = "0%";
 }
 
+/*
+ * Create an area for the image map with the coordinates of a room
+ * and add a click event listener to display the room popup
+ */
 function createArea(room) {
   let area = document.createElement('area');
   area.shape = 'poly';
@@ -24,17 +111,10 @@ function createArea(room) {
   document.getElementById('blueprint').appendChild(area);
 }
 
-function scaleCoordinates(array) {
-  var img = document.querySelector('.image img')
-  var scaleFactor = img.offsetWidth / img.naturalWidth;
-  var scaled = array.map(pair => {
-    return pair.map(c => {
-      return c * scaleFactor;
-    })
-  })
-  return scaled;
-}
-
+/*
+ * Prepare the room popup and calculate the correct position on the image
+ * to display it
+ */
 function showRoomPopUp(room, mouseX, mouseY) {
   let roomPopup = document.getElementById('roompopup');
   populateRoomPopup(room);
@@ -42,10 +122,8 @@ function showRoomPopUp(room, mouseX, mouseY) {
   let image = document.querySelector('.image img');
   let roomCenter = calculateCenter(room.coordinates);
 
-  let offset = 0;
-  if (window.matchMedia('(orientation:portrait)').matches) {
-    offset = (roomPopup.offsetWidth - roomPopup.offsetHeight) / 2;
-  }
+  // If the popup is rotated the coordinates need to be adjusted a bit
+  let offset = window.matchMedia('(orientation:portrait)').matches ? (roomPopup.offsetWidth - roomPopup.offsetHeight) / 2 : 0;
   
   let posX = roomCenter[0] < (image.offsetWidth / 2) ?
               Math.max(0 - offset, mouseX + offset - roomPopup.offsetWidth) :
@@ -59,6 +137,9 @@ function showRoomPopUp(room, mouseX, mouseY) {
   roomPopup.style.visibility = 'visible';
 }
 
+/*
+ * Fill in the roompopup div with room name and devices
+ */
 function populateRoomPopup(room) {
   let title = document.getElementById('roomname');
   title.textContent = room.name;
@@ -72,6 +153,9 @@ function populateRoomPopup(room) {
   });
 }
 
+/*
+ * Create an element for a device to be added to the device list
+ */
 function createDeviceElement(device) {
   let p = document.createElement("p");
   p.id = "device-" + device.id;
@@ -91,77 +175,27 @@ function createDeviceElement(device) {
   return p;
 }
 
-function getAll(endpoint, params = "", callback) {
-  let uri = API_BASE + endpoint + ".php" + params;
-  doGet(uri, callback);
+  /*
+   * Math calculations
+   */
+
+/*
+ * Transform the coordinates if the displayed image is not in the original size
+ */
+function scaleCoordinates(array) {
+  var img = document.querySelector('.image img')
+  var scaleFactor = img.offsetWidth / img.naturalWidth;
+  var scaled = array.map(pair => {
+    return pair.map(c => {
+      return c * scaleFactor;
+    })
+  })
+  return scaled;
 }
 
-function doGet(uri, callback) {
-  let xhr = new XMLHttpRequest();
-  xhr.open("GET", uri, true);
-  doRequest(xhr, callback);
-}
-
-function doPost(uri, body, callback) {
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", uri, true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-  doRequest(xhr, callback, body);
-}
-
-function doPut(uri, body, callback) {
-  let xhr = new XMLHttpRequest();
-  xhr.open("PUT", uri, true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-  doRequest(xhr, callback, body);
-}
-
-function doDelete(uri, callback) {
-  let xhr = new XMLHttpRequest();
-  xhr.open("DELETE", uri, true);
-  doRequest(xhr, callback);
-}
-
-function doRequest(xhr, callback, body = null) {
-  xhr.setRequestHeader("Accept", "application/json");
-  xhr.send(body);
-  xhr.onreadystatechange = function() {
-    if (this.readyState == 4) {
-      callback(this.status, this.response);
-    }
-  }
-}
-
-function setStatus(deviceId, status) {
-  let data = { "id": deviceId, "status": status };
-  let uri = API_BASE + "status.php"
-  doPost(uri, JSON.stringify(data), () => { return; });
-}
-
-function getStatus(deviceId) {
-  return sessionStorage.getItem('device-' + deviceId);
-}
-
-function login() {
-  let json = { username: document.getElementById("username").value, password: document.getElementById("password").value };
-  let uri = PROJECT_ROOT + "/api/auth.php";
-  
-  doPost(uri, JSON.stringify(json), (status) => {
-    if (status == 204) {
-      window.location.assign(PROJECT_ROOT);
-    } else {
-      let p = document.getElementById("error");
-      if (p === null || p === undefined) {
-        p = document.createElement("p");
-        p.id = "error";
-        p.class = "error";
-        p.appendChild(document.createTextNode("Invalid username and password"));
-        document.getElementById("login").appendChild(p);
-      }
-    }
-  });
-}
-
+/*
+ * Calculate the center point of a set of coordinates
+ */
 function calculateCenter(coordinates) {
   let center = [];
   if (coordinates.length < 4) {
@@ -184,55 +218,112 @@ function calculateCenter(coordinates) {
   return center;
 }
 
-function startSse() {
-  var events = new EventSource(API_BASE + "events.php");
-  events.onmessage = (event) => {
-    console.log(event.data);
-    let data = JSON.parse(event.data);
-    sessionStorage.setItem('device-' + data.id, data.status);
-    let p = document.getElementById("device-" + data.id);
-    if (p !== null) {
-      p.getElementsByTagName("input")[0].value = data.status;
+  /*
+   * Server communication
+   */
+
+/*
+ * Send device update to server
+ */
+function setStatus(deviceId, status) {
+  let data = { "id": deviceId, "status": status };
+  let uri = API_BASE + "status.php"
+  doPost(uri, JSON.stringify(data), () => { return; });
+}
+
+/*
+ * Format the values from the login form as JSON and send to the auth endpoint
+ */
+function login() {
+  let json = { username: document.getElementById("username").value, password: document.getElementById("password").value };
+  let uri = PROJECT_ROOT + "/api/auth.php";
+  
+  doPost(uri, JSON.stringify(json), (status) => {
+    if (status == 204) {
+      window.location.assign(PROJECT_ROOT);
+    } else {
+      let p = document.getElementById("error");
+      if (p === null || p === undefined) {
+        p = document.createElement("p");
+        p.id = "error";
+        p.class = "error";
+        p.appendChild(document.createTextNode("Invalid username or password"));
+        document.getElementById("login").appendChild(p);
+      }
+    }
+  });
+}
+
+  /*
+   * Ajax helper functions
+   */
+
+/*
+ * Get all rooms/devices/roomtypes/devicetypes/users
+ */
+function getAll(endpoint, params = "", callback) {
+  let uri = API_BASE + endpoint + ".php" + params;
+  doGet(uri, callback);
+}
+
+/*
+ * Make a GET request
+ */
+function doGet(uri, callback) {
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", uri, true);
+  doRequest(xhr, callback);
+}
+
+/*
+ * Make a POST request
+ */
+function doPost(uri, body, callback) {
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", uri, true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  doRequest(xhr, callback, body);
+}
+
+/*
+ * Make a PUT request
+ */
+function doPut(uri, body, callback) {
+  let xhr = new XMLHttpRequest();
+  xhr.open("PUT", uri, true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  doRequest(xhr, callback, body);
+}
+
+/*
+ * Make a DELETE request
+ */
+function doDelete(uri, callback) {
+  let xhr = new XMLHttpRequest();
+  xhr.open("DELETE", uri, true);
+  doRequest(xhr, callback);
+}
+
+/*
+ * Send request and execute callback function after response
+ */
+function doRequest(xhr, callback, body = null) {
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.send(body);
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4) {
+      callback(this.status, this.response);
     }
   }
 }
 
-function init() {
-  getAll("rooms", "?includeDevices=true", (status, data) => {
-    if (status == 200) {
-      sessionStorage.setItem('rooms', data);
-      let rooms = JSON.parse(data);
-      rooms.forEach(room => {
-        createArea(room);
-      });
-      startSse();
-    }
-  });
+  /*
+   * Other helper functions
+   */
 
-  window.addEventListener('resize', (e) => {
-    let mapElem = document.querySelector('.image map');
-    while (first = mapElem.firstChild) {
-      mapElem.removeChild(first);
-    }
-    let rooms = JSON.parse(sessionStorage.getItem('rooms'));
-    rooms.forEach(room => {
-      createArea(room);
-    });
-  });
-
-  document.getElementById('closepopup')?.addEventListener('click', () => {
-    document.getElementById('roompopup').style.visibility = 'hidden';
-  });  
-
-  let loginForm = document.getElementById("login");
-  if (loginForm !== null) {
-    loginForm.querySelector('#submit').addEventListener('click', login);
-    document.addEventListener('keypress', function(ev) { ev.key == 'Enter' ? login() : null; });
-  }
-  document.getElementById('open')?.addEventListener('click', openBar);
-  document.getElementById('close')?.addEventListener('click', closeBar);
+/*
+ * Get status for a specified device from browser storage
+ */
+function getStatus(deviceId) {
+  return sessionStorage.getItem('device-' + deviceId);
 }
-
-window.addEventListener('load', () => {
-  init();
-});
