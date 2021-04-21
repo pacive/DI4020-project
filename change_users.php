320a03147d2d0d7d2de35e329fc5683335742e2b
Linnea Wall <llinneawall@gmail.com>
2021-04-21 16:21:01 +0200

    edit/create new users

---
+++ change_users.php
@@ -0,0 +1,11 @@
+<?php
+require_once('template.php');
+?>
+
+<h3>What do you want to do? </h3>
+<button href="edit_current_user.php">Edit current user</button>
+<button href="new_user.php">Add new user</button>
+
+</div>
+</body>
+</html>
